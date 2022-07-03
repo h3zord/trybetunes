6ac978cc1e9f5b4803a1c7437a0b1dcc73711bc6
Lucas <lucaschmain@gmail.com>
2022-07-03 14:48:28 -0300

    concluindo requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,52 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      checkInputLength: true,
+    };
+  }
+
+  updateState = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => { this.checkInputValueLength(); });
+  }
+
+  checkInputValueLength = () => {
+    const { search } = this.state;
+    const minLength = 2;
+    if (search.length >= minLength) this.setState({ checkInputLength: false });
+    else this.setState({ checkInputLength: true });
+  }
+
   render() {
+    const { checkInputLength, search } = this.state;
+    const { updateState } = this;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="search"
+          data-testid="search-artist-input"
+          placeholder="Digite aqui"
+          onChange={ updateState }
+          name="search"
+          value={ search }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ checkInputLength }
+        >
+          Pesquisar
+        </button>
+
       </div>
     );
   }
